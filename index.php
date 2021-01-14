bc921752daf6f777fde7b23391c9ae7365dfda7b
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-14 10:47:38 +0400

    footer + header dans fichier à part

--- index.php
+++ index.php
@@ -19,36 +19,7 @@
 </head>
 
 <body>
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow fixed-top">
-        <div class="container-fluid col-md-8 mx-auto">
-            <a class="navbar-brand" href="#"><img id="logo_nav" src="src/ressources/img/logo-RW-reunion.png" alt="logo-RW-reunion"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav ms-auto me-3 mb-2 mb-lg-0 flex-end">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Nos prestations
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Conseil en Evolution Professionnelle</a></li>
-                            <li><a class="dropdown-item" href="#">Accélèr'Emploi</a></li>
-                            <li><a class="dropdown-item" href="#">Atelier Conseil</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Contactez-Nous</a>
-                    </li>
-                </ul>
-                <button type="button" class="btn btn-primary btn-green-nav">Connexion</button>
-            </div>
-        </div>
-    </nav>
+    <?php include 'src/includes/header.php'; ?>
 
     <section class="main">
         <div class="container-fluid d-flex flex-column align-items-center">
@@ -149,34 +120,7 @@
         </div>
     </section>
 
-    <footer>
-        <div class="logo-footer d-flex align-items-center justify-content-center">
-            <a href="">
-                <i class="fab fa-facebook icon-footer"></i>
-            </a>
-            <a href="">
-                <i class="fab fa-linkedin icon-footer"></i>
-            </a>
-            <a href="">
-                <img class="img-footer" src="src/ressources/img/france-competences.png" alt="france_competence_logo">
-            </a>
-            <a href="">
-                <img class="img-footer" src="src/ressources/img/pole_emploi.jpg" alt="pole_emploi_logo">
-            </a>
-        </div>
-        <hr class="w-75 mx-auto bg-light">
-        <div class="menu-footer d-flex w-75 mx-auto">
-            <p class="text-uppercase me-5">
-                <a class="nav-link text-white" href="">acccueil</a>
-            </p>
-            <p class="text-uppercase me-5">
-                <a class="nav-link text-white" href="">nos prestations</a>
-            </p>
-            <p class="text-uppercase">
-                <a class="nav-link text-white" href="">contact</a>
-            </p>
-        </div>
-    </footer>
+    <?php include 'src/includes/footer.php'; ?>
 </body>
 
 </html>
---
+++ src/includes/footer.php
@@ -0,0 +1,28 @@
+<footer>
+        <div class="logo-footer d-flex align-items-center justify-content-center">
+            <a href="">
+                <i class="fab fa-facebook icon-footer"></i>
+            </a>
+            <a href="">
+                <i class="fab fa-linkedin icon-footer"></i>
+            </a>
+            <a href="">
+                <img class="img-footer" src="src/ressources/img/franceCompetences.png" alt="france_competence_logo">
+            </a>
+            <a href="">
+                <img class="img-footer" src="src/ressources/img/poleEmploi.jpg" alt="pole_emploi_logo">
+            </a>
+        </div>
+        <hr class="w-75 mx-auto bg-light">
+        <div class="menu-footer d-flex w-75 mx-auto">
+            <p class="text-uppercase me-5">
+                <a class="nav-link text-white" href="">acccueil</a>
+            </p>
+            <p class="text-uppercase me-5">
+                <a class="nav-link text-white" href="">nos prestations</a>
+            </p>
+            <p class="text-uppercase">
+                <a class="nav-link text-white" href="">contact</a>
+            </p>
+        </div>
+    </footer>
