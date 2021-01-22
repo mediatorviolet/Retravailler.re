c1ba13d4f4281bba51203f87d80e3601e4714d9c
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-01-22 11:28:58 +0400

    affichage atelier dans acceleremploi et evo pro

--- src/pages/evolution-pro-atelier.php
+++ src/pages/evolution-pro-atelier.php
@@ -3,62 +3,36 @@
 <div class="container-fluid p-lg-5 p-md-3 main">
     <h2 class="display-4 text-center px-lg-5 py-lg-4 p-md-3 py-3">NOS ATELIERS</h2>
     <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This card has supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Choisissez une période</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-                <div class="card-footer text-end">
-                    <button type="submit" class="btn btn-primary btn-green-nav">S'inscrire</button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This card has supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Choisissez une période</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-                <div class="card-footer text-end">
-                    <button type="submit" class="btn btn-primary btn-green-nav">S'inscrire</button>
+    <?php
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=retravailler_final;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        $reponse = $bdd->query('SELECT * FROM atelier WHERE id_prestation = 1');
+        while ($donnees = $reponse->fetch()) { ?>
+            <div class="col">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= $donnees['nom'] ?></h5>
+                        <p class="card-text" style="text-align: justify;">
+                            <?= $donnees['description'] ?>
+                        </p>
+                        <select class="form-select" aria-label="Default select example">
+                            <option selected>Choisissez une date</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div class="card-footer text-end">
+                        <button type="submit" class="btn btn-primary btn-green-nav">S'inscrire</button>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                        This card has supporting text below as a natural lead-in to additional content.
-                    </p>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Choisissez une période</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
-                <div class="card-footer text-end">
-                    <button type="submit" class="btn btn-primary btn-green-nav">S'inscrire</button>
-                </div>
-            </div>
-        </div>
+        <?php }
+        $reponse->closeCursor();
+        ?>
     </div>
 </div>
