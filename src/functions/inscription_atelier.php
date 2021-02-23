d4ae3d07ce570f8d4213465d8bcd08358936a093
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-23 12:43:42 +0400

    un utilisateur ne peux pas s'inscrire plusieurs fois au même atelier

--- src/functions/inscription_atelier.php
+++ src/functions/inscription_atelier.php
@@ -8,12 +8,17 @@ function inscriptionAtelier()
         } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
-    
-        $req = $bdd->prepare("INSERT INTO association_user_date (id_user, id_dateAtelier) VALUES (:id_user, :id_dateAtelier)");
-    
-        $req->execute(array(
-            'id_user' => $_SESSION['user']['id_user'],
-            'id_dateAtelier' => $_POST['date']
-        ));
+
+        $request = $bdd->query('SELECT id_user FROM association_user_date WHERE id_user = "' . $_SESSION['user']['id_user'] . '" AND id_dateAtelier = "' . $_POST['date'] . '"');
+        if (empty($request)) {
+            $req = $bdd->prepare("INSERT INTO association_user_date (id_user, id_dateAtelier) VALUES (:id_user, :id_dateAtelier)");
+
+            $req->execute(array(
+                'id_user' => $_SESSION['user']['id_user'],
+                'id_dateAtelier' => $_POST['date']
+            ));
+
+            $bdd->query('UPDATE date_atelier SET nb_place = nb_place - 1 WHERE id_dateAtelier = "' . $_POST['date'] . '"');
+        }
     }
 }
