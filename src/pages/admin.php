eaa75be48e5df37f24fab1ec243a3a567edf0f90
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-24 08:58:23 +0400

    modale pour voir la liste des inscrits

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -2,9 +2,11 @@
     <h2 class="display-4 text-center px-lg-5 py-lg-5 p-md-3 py-3">Administrateur</h2>
 
     <div class="d-flex justify-content-center mb-5">
-        <button type="submit" class="btn btn-primary btn-green-nav" name="creer_atelier">
-            Créer un atelier
-        </button>
+        <form action="" method="post">
+            <button type="submit" class="btn btn-primary btn-green-nav" name="creer_atelier">
+                Créer un atelier
+            </button>
+        </form>
     </div>
     <hr>
     <h3 class="display-5 text-center px-lg-5 p-md-3 py-3">Ateliers crées :</h3>
@@ -15,7 +17,34 @@
                     <h5 class="card-title">Nom de l'atelier</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p>Nombre de places restantes :</p>
-                    <a href="">Voir la liste des utilisateurs inscrits</a>
+                    <!-- Button trigger modal -->
+                    <a href="" data-bs-toggle="modal" data-bs-target="#modal1">Voir la liste des utilisateurs inscrits</a>
+                    <!-- Modal -->
+                    <div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <ul>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                    </ul>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary btn-green-nav">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
                 <div class="card-footer text-end">
                     <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
@@ -33,7 +62,33 @@
                     <h5 class="card-title">Nom atelier</h5>
                     <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     <p>Nombre de places restantes :</p>
-                    <a href="">Voir la liste des utilisateurs inscrits</a>
+                    <!-- Button trigger modal -->
+                    <a href="" data-bs-toggle="modal" data-bs-target="#modal2">Voir la liste des utilisateurs inscrits</a>
+                    <!-- Modal -->
+                    <div class="modal fade" id="modal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    <ul>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                        <li>A</li>
+                                    </ul>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary btn-green-nav">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="card-footer text-end">
                     <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
