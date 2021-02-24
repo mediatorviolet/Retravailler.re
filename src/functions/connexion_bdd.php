f7b9fd0487d052d293d2bfbfbb45353c8d8176d4
leo599simplon <leo599simplon@gmail.com>
2021-02-24 14:52:24 +0400

    creation page connexion bdd : modifications page ajout_utilisateur etconnexion

---
+++ src/functions/connexion_bdd.php
@@ -0,0 +1,28 @@
+<?php
+
+
+if ($_SERVER["REMOTE_ADDR"]=="127.0.0.1")
+{
+    $serveur = "127.0.0.1";
+    $dbname = "retravailler_final";
+    $user = "root";
+    $password = "";
+}
+
+else
+{
+    $serveur = "us-cdbr-east-03.cleardb.com";
+    $dbname = "heroku_7c2ac3a00a455e9";
+    $user = "be460bc87bab43";
+    $password = "2fe501de";
+}
+
+try {
+    //On se connecte à la BDD
+    $bdd = new PDO("mysql:host=$serveur;dbname=$dbname", $user, $password);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_log(date('l jS \of F Y h:i:s A') . ": Connexion à la base de données réussie\r\n", 3, 'src/var/log.txt');
+} catch (PDOException $e) { // Si erreur, on renvoi un message d'erreur
+    error_log(date('l jS \of F Y h:i:s A') . ": Connexion à la base de données impossible\r\n", 3, 'src/var/log.txt');
+    echo 'Erreur : ' . $e->getMessage();
+}
