e334bbc1c1a62eb5ec81240cd0018606609a7883
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-24 15:35:14 +0400

    Affichage des ateliers crées dans dashboard admin

--- src/pages/admin.php
+++ src/pages/admin.php
@@ -2,101 +2,103 @@
     <h2 class="display-4 text-center px-lg-5 py-lg-5 p-md-3 py-3">Administrateur</h2>
 
     <div class="d-flex justify-content-center mb-5">
-            <a href="index.php?page=creation_atelier" class="btn btn-primary btn-green-nav" name="creer_atelier">
-                Créer un atelier
-            </a>
+        <a href="index.php?page=creation_atelier" class="btn btn-primary btn-green-nav">
+            Créer un atelier
+        </a>
     </div>
     <hr>
     <h3 class="display-5 text-center px-lg-5 p-md-3 py-3">Ateliers crées :</h3>
     <div class="row row-cols-1 row-cols-md-2 g-4">
-        <div class="col">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Nom de l'atelier</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p>Nombre de places restantes :</p>
-                    <!-- Button trigger modal -->
-                    <a href="" data-bs-toggle="modal" data-bs-target="#modal1">Voir la liste des utilisateurs inscrits</a>
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <ul>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                    </ul>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary btn-green-nav">Save changes</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Prestation</th>
+                    <th scope="col">Date</th>
+                    <th scope="col">Titre</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Places restantes</th>
+                    <th scope="col">Liste utilisateurs</th>
+                    <th scope="col">Modifier</th>
+                    <th scope="col">Désactiver</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                try {
+                    $bdd = new PDO('mysql:host=127.0.0.1;dbname=retravailler_final;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                } catch (Exception $e) {
+                    die('Erreur : ' . $e->getMessage());
+                }
 
-                </div>
-                <div class="card-footer text-end">
-                    <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
-                        Modifier
-                    </button>
-                    <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
-                        Désactiver
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card h-100">
-                <div class="card-body">
-                    <h5 class="card-title">Nom atelier</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p>Nombre de places restantes :</p>
-                    <!-- Button trigger modal -->
-                    <a href="" data-bs-toggle="modal" data-bs-target="#modal2">Voir la liste des utilisateurs inscrits</a>
-                    <!-- Modal -->
-                    <div class="modal fade" id="modal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <ul>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                        <li>A</li>
-                                    </ul>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary btn-green-nav">Save changes</button>
+                $sql = 'SELECT a.id_atelier, a.nom, a.description, d.id_dateAtelier, d.id_atelier, d.date_atelier, d.nb_place, d.id_prestation FROM atelier a RIGHT JOIN date_atelier d ON a.id_atelier = d.id_atelier ORDER BY d.date_atelier';
+                $req = $bdd->query($sql);
+                $datas = $req->fetchAll();
+                foreach ($datas as $data) {
+                ?>
+                    <tr>
+                        <td>
+                            <?php
+                            switch ($data['id_prestation']) {
+                                case 1:
+                                    echo 'Conseil en Evolution Professionnelle';
+                                    break;
+                                case 2:
+                                    echo 'Accélèr\'Emploi';
+                                    break;
+                                case 3:
+                                    echo 'Atelier Conseil';
+                                    break;
+                            }
+                            ?>
+                        </td>
+                        <td><?= $data['date_atelier'] ?></td>
+                        <td><?= $data['nom'] ?></td>
+                        <td><?= substr($data['description'], 0, 20) ?>...</td>
+                        <td><?= $data['nb_place'] ?></td>
+                        <td>
+                            <!-- Button trigger modal -->
+                            <a href="#" data-bs-toggle="modal" data-bs-target="#modal<?= $data['id_dateAtelier'] ?>">Voir</a>
+                            <!-- Modal -->
+                            <div class="modal fade" id="modal<?= $data['id_dateAtelier'] ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                        <div class="modal-body">
+                                            <ul>
+                                                <?php
+                                                $sql = 'SELECT * FROM association_user_date a JOIN user u ON a.id_user = u.id_user WHERE id_dateAtelier = "' . $data['id_dateAtelier'] . '"';
+                                                $req = $bdd->query($sql);
+                                                $results = $req->fetchAll();
+                                                foreach ($results as $result) {
+                                                ?>
+                                                    <li><?= $result['prenom'] . ' ' . $result['nom'] ?></li>
+                                                <?php } ?>
+                                            </ul>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary btn-green-nav">Save changes</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-footer text-end">
-                    <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
-                        Modifier
-                    </button>
-                    <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
-                        Désactiver
-                    </button>
-                </div>
-            </div>
-        </div>
+                        </td>
+                        <td>
+                            <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
+                                Modifier
+                            </button>
+                        </td>
+                        <td>
+                            <button type="submit" class="btn btn-primary btn-green-nav" name="inscription_atelier">
+                                Désactiver
+                            </button>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
 </div>
