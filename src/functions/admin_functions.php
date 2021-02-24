fc0d42579d30740132a06524534643cc18b0326e
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-24 15:34:52 +0400

    Création atelier admin

---
+++ src/functions/admin_functions.php
@@ -0,0 +1,39 @@
+<?php
+
+function creationAtelier()
+{
+    if (isset($_POST['creer_atelier'])) {
+        try {
+            $bdd = new PDO('mysql:host=127.0.0.1;dbname=retravailler_final;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+        } catch (Exception $e) {
+            die('Erreur : ' . $e->getMessage());
+        }
+
+        extract($_POST);
+        $sql_atelier = 'INSERT INTO atelier (id_prestation, nom, description) VALUES (:id_prestation, :nom, :description)';
+        $req_atelier = $bdd->prepare($sql_atelier);
+
+        $req_atelier->execute(array(
+            'id_prestation' => $_POST['prestation'],
+            'nom' => $_POST['titre'],
+            'description' => $_POST['description']
+        ));
+
+        $sql_date = 'INSERT INTO date_atelier (id_atelier, date_atelier, nb_place, id_prestation) VALUES (:id_atelier, :date_atelier, :nb_place, :id_prestation)';
+        $req_date = $bdd->prepare($sql_date);
+
+        $sql_idAtelier = 'SELECT MAX(id_atelier) FROM atelier';
+        $req_idAtelier = $bdd->query($sql_idAtelier);
+        $data = $req_idAtelier->fetchColumn();
+        for ($i = 0; $i < (count($_POST) - 8); $i++) {
+            $req_date->execute(array(
+                'id_atelier' => $data,
+                'date_atelier' => $_POST['date' . $i] . ' ' . $_POST['heure' . $i],
+                'nb_place' => $_POST['nb_place' . $i],
+                'id_prestation' => $_POST['prestation']
+            ));
+        }
+
+        header('Location: index.php?page=admin');
+    }
+}
--- src/pages/admin_creaAtelier.php
+++ src/pages/admin_creaAtelier.php
@@ -1,11 +1,12 @@
 <?php
 include 'src/functions/admin_functions.php';
+creationAtelier()
 ?>
 
 <div class="container-fluid p-lg-5 p-md-3">
     <h2 class="display-4 text-center px-lg-5 py-lg-5 p-md-3 py-3">Création atelier</h2>
     <div>
-        <form action="" method="POST" class="row g-3 col-md-10 mx-auto">
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST" class="row g-3 col-md-10 mx-auto">
             <div class="col-12">
                 <label for="prestation" class="form-label">Prestation</label>
                 <select name="prestation" id="" class="form-select">
@@ -23,21 +24,21 @@ include 'src/functions/admin_functions.php';
                 <label for="description" class="form-label">Description</label>
                 <textarea name="description" id="description" class="form-control" cols="30" rows="10"></textarea>
             </div>
-            <div id="div1" class="mt-5">
+            <div id="div0" class="mt-5">
                 <div class="input-group col-md-6">
                     <span class="input-group-text">Date et heure</span>
-                    <input type="date" aria-label="First name" class="form-control" name="date1">
-                    <input type="time" aria-label="Last name" class="form-control" name="heure1">
+                    <input type="date" aria-label="First name" class="form-control" name="date0">
+                    <input type="time" aria-label="Last name" class="form-control" name="heure0">
                 </div>
                 <div class="col-md-6">
                     <label for="nb_place1" class="form-label">Nombre de places</label>
-                    <input type="number" class="form-control" name="nb_place1" min="1">
+                    <input type="number" class="form-control" name="nb_place0" min="1">
                 </div>
             </div>
             <div>
                 <p id="ajout_date" class="d-inline">Ajouter une date</p>
             </div>
-            <div class="col-12">
+            <div class="col-12 text-end">
                 <button type="submit" class="btn btn-primary btn-green-nav" name="creer_atelier">Créer</button>
             </div>
         </form>
