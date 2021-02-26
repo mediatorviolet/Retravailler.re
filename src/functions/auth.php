d3825002307257903ce3303c755d564b9a174a79
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-02-26 09:51:47 +0400

    Commentaires auth.php

--- src/functions/auth.php
+++ src/functions/auth.php
@@ -1,19 +1,27 @@
 <?php
 
+/**
+ * 
+ * Authentification des utilisateurs
+ * Cette class sera appelée sur chaque page nécessitant une authentification
+ * 
+ */
 class Auth
 {
     static function isLogged()
     {
+        // On vérifie l'existence de la variable $_SESSION et qu'un 'email' et un 'password' y sont associés
         if (isset($_SESSION['user']) && isset($_SESSION['user']['email']) && isset($_SESSION['user']['password'])) {
             extract($_SESSION['user']);
-            include 'src/functions/connexion_bdd.php';
+            include 'src/functions/connexion_bdd.php'; // Connexion à la BDD
 
+            // On vérifie que l'email et le password présent dans la variable $_SESSION correpsondent à ceux dans la BDD
             $req = $bdd->query("SELECT id_user FROM user WHERE email = '$email' AND password = '$password'");
             $res = $req->fetch();
 
-            if ($res) {
+            if ($res) { // Si les identifiants correspondent, on renvoie true
                 return true;
-            } else {
+            } else { // Sinon on renvoit false
                 return false;
             }
         }
